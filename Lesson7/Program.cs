4028c859e2a457e3acc8012963e95e998cf9be01
carantoin <90127329+carantoin@users.noreply.github.com>
2024-10-21 15:06:03 +0200

    Initial commit

---
+++ Lesson7/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.WriteLine("Введите количество минут:");
+    int min = int.Parse(Console.ReadLine());
+    
+    int b = 1;
+    int bacteriaCount = b * (int)Math.Pow(2, min);
+    
+    Console.WriteLine($"Через {min} минут количество бактерий будет: {bacteriaCount}");
+}
+catch
+{
+    Console.WriteLine("Введите правильные данные!");
+}
